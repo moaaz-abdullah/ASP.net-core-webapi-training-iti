d0b9672ae38da3e79109a93065ac2c62e7f476a6
moaaz-abdullah <moaaz200026@gmail.com>
2026-07-26 00:25:05 +0300

    Add generic repository for CRUD operations
    Introduced GenericRepository<TEntity> in WebApplicationDay1.Repository to provide reusable CRUD methods for any entity using Entity Framework Core and ITIContext.

---
+++ WebApplicationDay1/Repository/GenericRepoForStudents.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationDay1.Models;
+
+namespace WebApplicationDay1.Repository
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        private readonly ITIContext db;
+
+        public GenericRepository(ITIContext db)
+        {
+            this.db = db;
+        }
+
+        public List<TEntity> GetAll()
+        {
+            return db.Set<TEntity>().ToList();
+        }
+
+        public TEntity? GetById(object id)
+        {
+            return db.Set<TEntity>().Find(id);
+        }
+
+        public void Add(TEntity entity)
+        {
+            db.Set<TEntity>().Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            db.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+            var entity = db.Set<TEntity>().Find(id);
+
+            if (entity != null)
+            {
+                db.Set<TEntity>().Remove(entity);
+            }
+        }
+
+        public void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+    }
+}
