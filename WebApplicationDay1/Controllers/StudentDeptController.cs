7d976c3896b40a616702d6b49e01361673c7477a
moaaz-abdullah <moaaz200026@gmail.com>
2026-07-27 23:26:15 +0300

    Add StudentDeptController and Unit of Work implementation
    Implemented StudentDeptController with an API endpoint to add students and their departments using the Unit of Work pattern. Added a UOW class to manage repositories and database commits. Updated using directives and namespaces for dependency injection and repository usage.

---
+++ WebApplicationDay1/Controllers/StudentDeptController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationDay1.Repository;
+using WebApplicationDay1.Controllers;
+using WebApplicationDay1.Models;
+using WebApplicationDay1.UnitOfWork;
+
+namespace WebApplicationDay1.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/[controller]")]
+    [ApiController]
+    public class StudentDeptController : ControllerBase
+    {
+        UOW unitOfWork;
+
+        public StudentDeptController(UOW unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public ActionResult add(Student student)
+        {
+            unitOfWork.departmentRepos.Add(student.Dept);
+            unitOfWork.studentRepos.Add(student);
+            unitOfWork.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ WebApplicationDay1/UnitOfWork/UOW.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using WebApplicationDay1.Models;
+using WebApplicationDay1.Repository;
+
+namespace WebApplicationDay1.UnitOfWork
+{
+    public class UOW
+    {
+        private readonly ITIContext db;
+
+        public GenericRepository<Student> studentRepos;
+        public GenericRepository<Department> departmentRepos;
+
+        public UOW(ITIContext db)
+        {
+            this.db = db;
+            studentRepos = new GenericRepository<Student>(db);
+            departmentRepos = new GenericRepository<Department>(db);
+        }
+
+        public void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+
+    }
+}
